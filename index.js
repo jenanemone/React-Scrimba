35fcb52bf6bf71f0300701d7ed89f39d6eaf62b1
Jennie Allen <jenanemone@gmail.com>
2022-09-14 23:56:25 -0500

    Better bare bones

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*
+If not using a CDN, import the libraries:
+import React from "react";
+import ReactDOM from "react-dom";
 
+*/
 
 ReactDOM.render(<h1>Welcome to my world, React!</h1>, document.getElementById("root"))
